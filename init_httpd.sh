39d3c9e8cc402a00c87929e09385e9f27691d27e
yst1979 <yst.guy.tw@gmail.com>
2021-07-17 17:14:56 +0800

    Update init_httpd.sh

--- init_httpd.sh
+++ init_httpd.sh
@@ -4,4 +4,5 @@ yum install httpd -y
 systemctl restart httpd
 systemctl enable httpd
 
-echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
+#echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
+echo "hi" > /var/www/html/index.html
