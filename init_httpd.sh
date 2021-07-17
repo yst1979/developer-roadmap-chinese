81e187aeba126a8acc5cb3299fbb9386a99c46f6
yst1979 <yst.guy.tw@gmail.com>
2021-07-17 17:24:45 +0800

    Update and rename init_httpd.sh to init_httpd3.sh

--- init_httpd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum install httpd -y
-systemctl restart httpd
-systemctl enable httpd
-
-#echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
-echo "hi" > /var/www/html/index.html
